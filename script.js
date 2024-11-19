9582c0281c6112fd5bf9ba8b3991eb2b02062894
anieshvohra <aniesh.vohra@edu.sait.ca>
2024-11-19 02:46:43 -0700

    ALERT BUTTON ADDED
    'Click Me' button was added as an alert to welcome people by using button id in index.html file and  using getElementById in script.js

--- script.js
+++ script.js
@@ -14,7 +14,13 @@ if (hour < 12) {
     greetingMessage = "Good evening!";
     greetingClass = "evening";
 }
+// **Alert Button: Functionality and Hover Effect**
+const alertButton = document.getElementById("btn-alert");
 
+// Add click event to display an alert
+alertButton.addEventListener("click", () => {
+    alert("Hello world! Welcome to my page. This webpage is all dedicated about me, know more about me by clicking on 'OK' and then 'BACKGROUND INFORMATION' above. ");
+});
 greetingDiv.textContent = greetingMessage;
 greetingDiv.className = greetingClass;
 // **Footer: Display Current Year**
